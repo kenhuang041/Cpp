a193ddddf255e8a6d226525f215a4369030b86de
kenhuang041 <kenhuang0401@gmail.com>
2025-07-13 11:57:28 +0800

    Update o713.cpp

--- zerojudge/apcs/p3/o713.cpp
+++ zerojudge/apcs/p3/o713.cpp
@@ -31,72 +31,72 @@ void bfs(pii start, int range) {
 	所以range要+1 queue也需要存當前可擴張格數 ^^ 
 	*/
 	queue<pair<pii,int>> q; //y軸,x軸,爆炸範圍 
-    q.push({start, range});
-    visited[start.F][start.S] = range;
+    	q.push({start, range});
+    	visited[start.F][start.S] = range;
     
 	while (!q.empty()) { //開始bfs
         pair<pii,int> cur = q.front();
-		q.pop();
+	q.pop();
         int y = cur.F.F, x = cur.F.S; //當前節點y,x軸 
-		int val = cur.S; //當前爆炸範圍 
+	int val = cur.S; //當前爆炸範圍 
 		
         for (int i = 0; i < 4; i++) { //檢查上下左右是否可繼續延伸 
             int ny = y + moves[0][i];
-			int nx = x + moves[1][i];
+	    int nx = x + moves[1][i];
 			
-			/*
-			判斷是否繼續bfs的條件:
-			   1.沒有超出陣列範圍
-			   2.不是牆壁
-			   3.此格的爆炸範圍是否大於原先的擴張格數
-			(擴張格數-1 = 此格的爆炸範圍) 
-			如果原先的擴張格數大於當前爆炸範圍 則代表先前以有爆炸經過 且範圍大於當前爆炸範圍
-			所以就不需要繼續bfs此點了^^ 算是一種優化 
-			*/
+		/*
+		判斷是否繼續bfs的條件:
+		  1.沒有超出陣列範圍
+		  2.不是牆壁
+		  3.此格的爆炸範圍是否大於原先的擴張格數
+		(擴張格數-1 = 此格的爆炸範圍) 
+		如果原先的擴張格數大於當前爆炸範圍 則代表先前以有爆炸經過 且範圍大於當前爆炸範圍
+		所以就不需要繼續bfs此點了^^ 算是一種優化 
+		*/
             if (!OUT && a[ny][nx]!=-1 && val-1>visited[ny][nx]) {
             	if (visited[ny][nx] == 0) t++; //總爆炸格數++ 
-            	
+            
             	if (a[ny][nx] > val - 1) { //是否碰到其他炸彈 且範圍大於當前爆炸範圍
-					visited[ny][nx] = a[ny][nx]+1;
-					q.push({{ny, nx}, a[ny][nx]+1}); //bfs此點 且將範圍改為炸彈的爆炸範圍 -> a[ny][nx] 
-				} 
+		    visited[ny][nx] = a[ny][nx]+1;
+		    q.push({{ny, nx}, a[ny][nx]+1}); //bfs此點 且將範圍改為炸彈的爆炸範圍 -> a[ny][nx] 
+		} 
                 else {
-					visited[ny][nx] = val - 1; 
-					q.push({{ny, nx}, val - 1}); //接著bfs 
-				}
-			} 
+		    visited[ny][nx] = val - 1; 
+		    q.push({{ny, nx}, val - 1}); //接著bfs 
+		}
+	    } 
         }	
     }
 } 
  
 signed main() {
-	IO;
+    IO;
     cin >> m >> n >> q;
     pii start;
     memset(visited,0,sizeof(visited));
     
     for(int i=0; i<m; i++) {
     	for(int j=0; j<n; j++) {
-    		cin >> a[i][j];
-    		if(a[i][j]==-2) { //存起始爆炸點 
-    			start.F = i;
-    			start.S = j;
-			}
-		}
+    	    cin >> a[i][j];
+    	    if(a[i][j]==-2) { //存起始爆炸點 
+    	        start.F = i;
+    		start.S = j;
+	    }
 	}
+    }
 	
-	int l=0,r=m*n;
-	while(l<=r) { //二分搜求出解 (0 <= l,r <= m*n)
-		int mid = (l+r)/2;
-		t=1;
-		memset(visited,0,sizeof(visited));
-		bfs(start,mid); //引爆炸彈 距離為 mid 
+    int l=0,r=m*n;
+    while(l<=r) { //二分搜求出解 (0 <= l,r <= m*n)
+	int mid = (l+r)/2;
+	t=1;
+	memset(visited,0,sizeof(visited));
+	bfs(start,mid); //引爆炸彈 距離為 mid 
 		
-		//縮小二分搜範圍 
-		if(t<q) l=mid+1;
-		else r=mid-1;  
-	} 
+	//縮小二分搜範圍 
+	if(t<q) l=mid+1;
+	else r=mid-1;  
+    } 
 	
-	cout << l << endl; //輸出答案 
+    cout << l << endl; //輸出答案 
     return 0;
 }
