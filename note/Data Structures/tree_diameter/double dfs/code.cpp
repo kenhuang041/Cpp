60c328929e2639c195c3bf13de694f91c600a37a
kenhuang041 <kenhuang0401@gmail.com>
2025-07-14 15:10:40 +0800

    Update code.cpp

--- note/Data Structures/tree_diameter/double dfs/code.cpp
+++ note/Data Structures/tree_diameter/double dfs/code.cpp
@@ -1,38 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define IO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-#define pii pair<int,int>
 #define int long long
 #define N 100005
-#define F first
-#define S second
 vector<int> tree[N];
 int mx = -1,mx_node;
 
 void dfs(int u,int parent,int t) {
-	for(auto v : tree[u]) {
-		if(v == parent) continue;
-		dfs(v,u,t+1);
-	}	
-	if(t > mx) {
-		mx_node = u;
-		mx = t;
-	}
+    for(auto v : tree[u]) {
+	if(v == parent) continue;
+	dfs(v,u,t+1);
+    }	
+    if(t > mx) {
+	mx_node = u;
+	mx = t;
+    }
 }
 
 signed main() {
-	IO;
-	int n;
-	cin >> n;
+    IO;
+    int n;
+    cin >> n;
 	
-	for(int i=0,a,b; i<n-1; i++) {
-		cin >> a >> b;
-		tree[a].push_back(b);
-		tree[b].push_back(a);
-	}
+    for(int i=0,a,b; i<n-1; i++) {
+	cin >> a >> b;
+	tree[a].push_back(b);
+	tree[b].push_back(a);
+    }
 	
-	dfs(0,-1,0);
-	dfs(mx_node,-1,0);
-	cout << mx;
-	return 0;
+    dfs(0,-1,0);
+    dfs(mx_node,-1,0);
+    cout << mx;
+    return 0;
 } 
