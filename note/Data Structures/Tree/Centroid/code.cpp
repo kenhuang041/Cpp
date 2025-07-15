f8a84deb932b98f1bb540cd69ce7cc40ee70fed3
kenhuang041 <kenhuang0401@gmail.com>
2025-07-15 20:49:24 +0800

    Update code.cpp

--- note/Data Structures/Tree/Centroid/code.cpp
+++ note/Data Structures/Tree/Centroid/code.cpp
@@ -8,38 +8,38 @@ int cost[N];
 int n;
 
 int dfs(int u,int parent) {
-	int t = 1,r = 0;
-	for(auto i : tree[u]) {
-		if(i == parent) continue;
-		r = dfs(i,u);
-		t += r;
-		cost[u] = max(cost[u],r);
-	}
+    int t = 1,r = 0;
+    for(auto i : tree[u]) {
+	if(i == parent) continue;
+	r = dfs(i,u);
+	t += r;
+	cost[u] = max(cost[u],r);
+    }
 	
-	cost[u] = max(cost[u],n-t);
-	return t;
+    cost[u] = max(cost[u],n-t);
+    return t;
 }
 
 signed main() {
-	IO;
-	cin >> n;
-	for(int i=0,a,b; i<n-1; i++) {
-		cin >> a >> b;
-		tree[a].push_back(b);
-		tree[b].push_back(a);
-	}
+    IO;
+    cin >> n;
+    for(int i=0,a,b; i<n-1; i++) {
+	cin >> a >> b;
+	tree[a].push_back(b);
+	tree[b].push_back(a);
+    }
 	
-	dfs(0,-1);
-	int mn = INT_MAX,ans = -1;
-	for(int i=0; i<n; i++) {
-		if(cost[i] < mn) {
-			mn = cost[i];
-			ans = i;
-		}
+    dfs(0,-1);
+    int mn = INT_MAX,ans = -1;
+    for(int i=0; i<n; i++) {
+	if(cost[i] < mn) {
+	    mn = cost[i];
+	    ans = i;
 	}
+    }
 	
-	cout << ans;
-	return 0;
+    cout << ans;
+    return 0;
 } 
 
 /*
