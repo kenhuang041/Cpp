6a87f6c9a5863e42888d5af2f55195123e8f0f64
kenhuang041 <kenhuang0401@gmail.com>
2025-07-17 14:32:33 +0800

    Update f315.cpp

--- zerojudge/apcs/p4/f315.cpp
+++ zerojudge/apcs/p4/f315.cpp
@@ -14,62 +14,62 @@ vector<pii> v;
 int a[N],tree[4*N],n,ans = 0;
 
 void build(int l,int r,int node) {
-	if(l == r) {
-		tree[node] = a[l];
-		return;
-	}
+    if(l == r) {
+	tree[node] = a[l];
+	return;
+    }
 	
-	int mid = (l+r)/2;
-	build(l,mid,lc);
-	build(mid+1,r,rc);
-	tree[node] = tree[lc] + tree[rc];
+    int mid = (l+r)/2;
+    build(l,mid,lc);
+    build(mid+1,r,rc);
+    tree[node] = tree[lc] + tree[rc];
 }
 
 int query(int l,int r,int ql,int qr,int node) { //查詢區間不變 
-	if(ql > r || qr < l) return 0;
-	if(ql <= l && r <= qr) return tree[node];  
+    if(ql > r || qr < l) return 0;
+    if(ql <= l && r <= qr) return tree[node];  
 
-	int mid = (l+r)/2;
-	return query(l, mid, ql, qr, lc) + query(mid + 1, r, ql, qr, rc);
+    int mid = (l+r)/2;
+    return query(l, mid, ql, qr, lc) + query(mid + 1, r, ql, qr, rc);
 }
 
 void modify(int l,int r,int node,pii val) {
-	if(l == r) {
-		tree[node] = val.F;
-		return;
-	}
+    if(l == r) {
+	tree[node] = val.F;
+	return;
+    }
 	
-	int mid = (l+r) / 2;
-	if(val.S <= mid) modify(l,mid,lc,val);
-	else modify(mid+1,r,rc,val);
-	
-	tree[node] = tree[lc] + tree[rc];
+    int mid = (l+r) / 2;
+    if(val.S <= mid) modify(l,mid,lc,val);
+    else modify(mid+1,r,rc,val);	
+    tree[node] = tree[lc] + tree[rc];
 }
 
 signed main() {
-	cin >> n;
-	memset(a,0,sizeof(a));
-	for(int i = 0; i < 2*n; i++) {
+    IO;
+    cin >> n;
+    memset(a,0,sizeof(a));
+    for(int i = 0; i < 2*n; i++) {
         int tmp;
         cin >> tmp;
-        v.push_back({tmp, i});
+	v.push_back({tmp, i});
     }
     sort(v.begin(), v.end());
     build(0, 2*n-1, 0);
 	
-	for(int i=0; i<v.size(); i+=2) {
-		int l = v[i].S,r = v[i+1].S;
-		if(l > r) swap(l, r);
+    for(int i=0; i<v.size(); i+=2) {
+	int l = v[i].S,r = v[i+1].S;
+	if(l > r) swap(l, r);
 		
-		if(l + 1 <= r - 1) {
+	if(l + 1 <= r - 1) {
             int count = query(0, 2*n-1, l+1, r-1, 0);
             ans += count;
         }
 		
-		modify(0,2*n-1,0,{1,l});
-		modify(0,2*n-1,0,{1,r});	
-	}
+	modify(0,2*n-1,0,{1,l});
+	modify(0,2*n-1,0,{1,r});	
+    }
 	
-	cout << ans;
+    cout << ans;
     return 0;
 }
