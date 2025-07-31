2153068a27d272a193b7b5a2d77fd25640213735
kenhuang041 <kenhuang0401@gmail.com>
2025-07-31 11:00:54 +0800

    Update code.cpp

--- note/algorithm/Sieve of Eratosthenes/code.cpp
+++ note/algorithm/Sieve of Eratosthenes/code.cpp
@@ -6,42 +6,42 @@ using namespace std;
 #define N 46341
 
 signed main() {
-	IO;
-	bool a[N];
-	int n;
-	vector<int> v;
-	memset(a,0,sizeof(a));
-	a[0] = 0;
-	a[1] = 0;
+    IO;
+    bool a[N];
+    int n;
+    vector<int> v;
+    memset(a,0,sizeof(a));
+    a[0] = 0;
+    a[1] = 0;
 	
-	for(int i=2; i<N; i++) {
-		if(!a[i]) {
-			v.push_back(i);
-			for(int j=i*i; j<N; j+=i) {
-				a[j] = 1;
-			}
-		}
+    for(int i=2; i<N; i++) {
+	if(!a[i]) {
+	    v.push_back(i);
+	    for(int j=i*i; j<N; j+=i) {
+		a[j] = 1;
+	    }
 	}
+    }
 	
-	while(cin >> n) {
-		if(n > N) {
-			bool no = 0;
-			for(auto it : v) {
-				if(pow(it,2) > n) break;
-				if(n%it == 0) {
-					no = 1;
-					break;
-				}
-			}
+    while(cin >> n) {
+	if(n > N) {
+	    bool no = 0;
+	    for(auto it : v) {
+		if(pow(it,2) > n) break;
+		if(n%it == 0) {
+		    no = 1;
+		    break;
+		}
+	    }
 			
-			if(!no) cout << "質數\n";
-			else cout << "非質數\n";
-		}
-		else {
-			if(!a[n]) cout << "質數\n";
-			else cout << "非質數\n";
-		}
+	    if(!no) cout << "質數\n";
+	    else cout << "非質數\n";
 	}
+	else {
+	    if(!a[n]) cout << "質數\n";
+	    else cout << "非質數\n";
+	}
+    }
     return 0;
 }
  
