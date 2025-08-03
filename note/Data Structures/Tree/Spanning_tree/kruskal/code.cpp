6fdf3f613480ed389107a20515db7520d87e77d1
kenhuang041 <kenhuang0401@gmail.com>
2025-08-03 21:30:05 +0800

    Update code.cpp

--- note/Data Structures/Tree/Spanning_tree/kruskal/code.cpp
+++ note/Data Structures/Tree/Spanning_tree/kruskal/code.cpp
@@ -5,7 +5,7 @@ using namespace std;
 #define int long long
 #define N 1005
 struct edge {
-	int a,b,w;
+    int a,b,w;
 };
 
 vector<int> dis;
@@ -14,54 +14,55 @@ vector<int> spanning_tree[N];
 int n,m;
 
 bool cmp(edge a,edge b) {
-	return a.w<b.w;
+    return a.w<b.w;
 }
 
 int search(int a) {
-	if(dis[a] == a) return a;
-	dis[a] = search(dis[a]);
-	return dis[a];
+    if(dis[a] == a) return a;
+    dis[a] = search(dis[a]);
+    return dis[a];
 }
 void connect(int a,int b) {
-	dis[search(a)] = search(b);
+    dis[search(a)] = search(b);
 }
 
 void kruskal() {
-	sort(tree.begin(),tree.end(),cmp);
-	dis.resize((n+1));
-	for(int i=0; i<n; i++) dis[i] = i;
+    sort(tree.begin(),tree.end(),cmp);
+    dis.resize((n+1));
+    for(int i=0; i<n; i++) dis[i] = i;
 	
-	int ans = 0,tmp = 0;
-	for(int i=0; i<m && tmp<n-1; i++) {
-		if(search(tree[i].a) != search(tree[i].b)) {
-			tmp++;
-			connect(tree[i].a,tree[i].b);
-			spanning_tree[tree[i].a].push_back(tree[i].b);
-			spanning_tree[tree[i].b].push_back(tree[i].a);
-			ans += tree[i].w;
-		}
+    int ans = 0,tmp = 0;
+    for(int i=0; i<m && tmp<n-1; i++) {
+	if(search(tree[i].a) != search(tree[i].b)) {
+	    tmp++;
+	    connect(tree[i].a,tree[i].b);
+	    spanning_tree[tree[i].a].push_back(tree[i].b);
+    	    spanning_tree[tree[i].b].push_back(tree[i].a);
+	    ans += tree[i].w;
 	}
+    }
 	
-	cout << ans << "\n";
+    cout << ans << "\n";
 }
 
 signed main() {
-	IO;
-	cin >> n >> m; //點 邊 
-	for(int i=0,a,b,w; i<m; i++) {
-		cin >> a >> b >> w;
-		tree.push_back({a,b,w});
-	} 
-	kruskal();
+    IO;
+    cin >> n >> m; //點 邊 
+    for(int i=0,a,b,w; i<m; i++) {
+	cin >> a >> b >> w;
+	tree.push_back({a,b,w});
+    } 
+    kruskal();
 	
-	for(int i=0; i<10; i++) {
-		cout << i << ": ";
-		for(auto it : spanning_tree[i]) {
-			cout << it << " ";
-		}
-		cout << "\n";
+    for(int i=0; i<10; i++) {
+	cout << i << ": ";
+	for(auto it : spanning_tree[i]) {
+	    cout << it << " ";
 	}
-	return 0;
+	cout << "\n";
+    }
+	
+    return 0;
 } 
 /*
 7 8
