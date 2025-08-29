79a8c6df22818355dae712964754570b17ffd149
kenhuang041 <kenhuang0401@gmail.com>
2025-08-29 12:11:17 +0800

    Update code.cpp

--- note/algorithm/Sieve of Eratosthenes/code.cpp
+++ note/algorithm/Sieve of Eratosthenes/code.cpp
@@ -15,32 +15,32 @@ signed main() {
     a[1] = 0;
 	
     for(int i=2; i<N; i++) {
-	if(!a[i]) {
-	    v.push_back(i);
-	    for(int j=i*i; j<N; j+=i) {
-		a[j] = 1;
+	    if(!a[i]) {
+	        v.push_back(i);
+	        for(int j=i*i; j<N; j+=i) {
+		        a[j] = 1;
+	        }
 	    }
-	}
     }
 	
     while(cin >> n) {
-	if(n > N) {
-	    bool no = 0;
-	    for(auto it : v) {
-		if(pow(it,2) > n) break;
-		if(n%it == 0) {
-		    no = 1;
-		    break;
-		}
-	    }
+	    if(n > N) {
+	        bool no = 0;
+	        for(auto it : v) {
+		        if(pow(it,2) > n) break;
+		        if(n%it == 0) {
+		            no = 1;
+		            break;
+		        }
+	        }
 			
-	    if(!no) cout << "質數\n";
-	    else cout << "非質數\n";
-	}
-	else {
-	    if(!a[n]) cout << "質數\n";
-	    else cout << "非質數\n";
-	}
+	        if(!no) cout << "質數\n";
+	        else cout << "非質數\n";
+	    }
+	    else {
+	        if(!a[n]) cout << "質數\n";
+	        else cout << "非質數\n";
+	    }
     }
     return 0;
 }
