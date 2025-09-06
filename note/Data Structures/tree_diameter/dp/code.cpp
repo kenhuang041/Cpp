b337133a606e595605071e2ab90e8cdd1b41e172
kenhuang041 <kenhuang0401@gmail.com>
2025-09-06 16:34:07 +0800

    Update code.cpp

--- note/Data Structures/tree_diameter/dp/code.cpp
+++ note/Data Structures/tree_diameter/dp/code.cpp
@@ -15,17 +15,17 @@ void dfs(int u,int parent) {
     int max1 = 0,max2 = 0;
 	
     for(auto v : tree[u]) {
-	if(v == parent) continue;
-	dfs(v,u);
-		
-	int d = dp[v]+1; //節點u到節點v的距離記得算 所以要+1 
-	if(d > max1) {
-	    max2 = max1;
-	    max1 = d;
-	} 
-	else if(d > max2) {
-    	    max2 = d;
-	}
+		if(v == parent) continue;
+		dfs(v,u);
+			
+		int d = dp[v]+1; //節點u到節點v的距離記得算 所以要+1 
+		if(d > max1) {
+		    max2 = max1;
+		    max1 = d;
+		} 
+		else if(d > max2) {
+	    	    max2 = d;
+		}
     }
 	
     dp[u] = max1;
@@ -39,8 +39,8 @@ signed main() {
 
     for(int i=0,a,b; i<n-1; i++) {
     	cin >> a >> b;
-	tree[a].push_back(b);
-	tree[b].push_back(a);
+		tree[a].push_back(b);
+		tree[b].push_back(a);
     }
 	
     dfs(0,-1);
